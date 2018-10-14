b43e8a95728263c856597b615fe8bf90b19148b1
Hiroyuki Yagihashi <h.yagihashi2424@icloud.com>
2018-10-14 08:19:10 +0000

    UserIndex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
+  before_action :require_user_logged_in, only: [:show, :followings, :followers, :likes]
   
   def index
     @users = User.search(params[:search]).page(params[:page])
